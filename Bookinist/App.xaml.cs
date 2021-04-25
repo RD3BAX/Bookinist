f56d67b91186fd115e9f758a865cb6d99cf28733
Denis Novikov <rd3bax@gmail.com>
2021-04-25 12:19:47 +0300

    Контейнер сервисов

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
 
 namespace Bookinist
 {
@@ -7,5 +10,30 @@ namespace Bookinist
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Host;
+
+        public static IHost Host => __Host 
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
---
+++ Bookinist/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Bookinist
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices);
+    }
+}
