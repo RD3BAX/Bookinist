39aa994eb1e03a31fffc57da116ad4ca251794d7
Denis Novikov <rd3bax@gmail.com>
2021-04-25 20:12:24 +0300

    Миграции

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Windows;
+using Bookinist.Data;
 using Bookinist.Services;
 using Bookinist.ViewModels;
 
@@ -20,6 +21,7 @@ namespace Bookinist
         public static IServiceProvider Services => Host.Services;
 
         internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services) => services
+            .AddDatabase(host.Configuration.GetSection("Database"))
             .AddServices()
             .AddViewModels()
         ;
