a38ce78ef715afe56125d0df2b8477097b9f4fe3
Denis Novikov <rd3bax@gmail.com>
2021-05-02 21:45:41 +0300

    Подготовка к выгрузке данных о статистике из БД

---
+++ Bookinist/Models/BestSellerInfo.cs
@@ -0,0 +1,11 @@
+﻿using Bookinist.DAL.Entities;
+
+namespace Bookinist.Models
+{
+    class BestSellerInfo
+    {
+        public Book Book { get; set; }
+
+        public int SellCount { get; set; }
+    }
+}
